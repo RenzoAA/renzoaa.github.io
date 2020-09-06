90d9f899b103e883aecfcfea2ea56b56353a1054
ralva <ralva@web.com>
2020-09-06 15:45:51 -0500

    Remodelando estilos

--- js/modules/revealelements.js
+++ js/modules/revealelements.js
@@ -16,13 +16,19 @@ export default function revealElements(animado,barraprogreso){
     for(var i = 0; i < barraProgreso.length; i++){
         let alturaBarra = barraProgreso[i].offsetTop;
         if(alturaBarra + 850 < scrollTop){
-            barraProgreso[i].style.background = '#34495E';
+            // barraProgreso[i].style.border = 'inherit';
             d.querySelector('.barra-progreso_html5').style.width = '85%';
+            d.querySelector('.barra-progreso_html5').style.backgroundColor = 'orange';
             d.querySelector('.barra-progreso_css').style.width = '90%';
+            d.querySelector('.barra-progreso_css').style.backgroundColor = 'steelblue';
             d.querySelector('.barra-progreso_sass').style.width = '70%';
+            d.querySelector('.barra-progreso_sass').style.backgroundColor = 'violet';
             d.querySelector('.barra-progreso_js').style.width = '80%';
+            d.querySelector('.barra-progreso_js').style.backgroundColor = 'yellow';
             d.querySelector('.barra-progreso_reactjs').style.width = '50%';
+            d.querySelector('.barra-progreso_reactjs').style.backgroundColor = 'skyblue';
             d.querySelector('.barra-progreso_nodejs').style.width = '40%';
+            d.querySelector('.barra-progreso_nodejs').style.backgroundColor = 'green';
         }else{
            barraProgreso[i].style.width = '0';
         }
